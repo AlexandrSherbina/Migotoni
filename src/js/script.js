9320ed35305fb70ef984b11d9c016452e4ae692c
Alexandr Sherbina <alex.sherbina.work@gmail.com>
2021-09-11 15:55:49 +0300

    chart fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,63 +1,33 @@
 // ==Chart============================================================//
-let densityCanvas = document.getElementById("densityChart");
+let densityCanvas = document.getElementById("chart-price");
 
 //Chart.defaults.global.defaultFontFamily = 'Lato';
-Chart.defaults.font.size = 24;
-let newNum = 20;
-let keys1 =( ( newNum -   15 ) / (15 * 100 / 100)) + 1;
-console.log( keys1.toFixed(4)); 
+Chart.defaults.font.size = 30;
+// let newNum = 20;
+// let keys1 =( ( newNum -   15 ) / (15 * 100 / 100)) + 1;
+// console.log( keys1.toFixed(4)); 
 
 
 let case1 = {
-  label: 'Кейс 14000 - 27999 грн',
-  data: [ 22.5, 33.5 ],
-  backgroundColor: 'rgba(0, 99, 132, 0.6)',
-  borderWidth: 0,
+  label: 'Изменение цены за месяц',
+  data: [ 0, 33.5, 80 ],
+  backgroundColor: 'rgba(48, 207, 208, 0.6)',
+  //background: "linear-gradient(180deg, #30CFD0 0%, #330867 100%)",
+  borderWidth: 1,
 //   yAxisID: "y-axis-density"
 };
 
-let case2 = {
-  label: 'Кейс 28 000 - 67 999 ГРН',
-  data: [14.4, 33.33],
-  backgroundColor: 'rgba(99, 132, 0, 0.6)',
-  borderWidth: 0,
-//   yAxisID: "y-axis-gravity"
-};
-
-let case3 = {
-  label: 'Кейс 68 000 - 139 999 ГРН',
-  data: [0.8, 44.44],
-  backgroundColor: 'rgba(246, 161, 65, 0.6)',
-  borderWidth: 0,
-//   yAxisID: "y-axis-gravity"
-};
-
-let case4 = {
-  label: 'Кейс 140 000 - 279 999 ГРН',
-  data: [0.5, 42.9],
-  backgroundColor: 'rgba(41, 35, 225, 0.6)',
-  borderWidth: 0,
-//   yAxisID: "y-axis-gravity"
-};
-
-let case5 = {
-  label: 'Кейс 280 000 ГРН',
-  data: [0.3, 40],
-  backgroundColor: 'rgba(225, 41, 35, 0.6)',
-  borderWidth: 0,
-//   yAxisID: "y-axis-gravity"
-};
 
 let casetData = {
   labels: ["Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", "Январь", "Февраль", "Март" , "Апрель", "Май", "Июнь"],
-  datasets: [case1, case2, case3, case4, case5]
+  datasets: [case1]
 };
 
 let chartOptions = {
   scales: {
     y: {
         ticks: {
-            // Include a dollar sign in the ticks
+        
             callback: function(value, index, values) {
                 return  value + '%';
             }
@@ -77,7 +47,7 @@ let chartOptions = {
         labels: {
             // This more specific font property overrides the global property
             font: {
-                size: 16
+                size: 30
             }
         }
     }
