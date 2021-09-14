8d193c9ce6e38783848443173c80617953b4665d
Alexandr Sherbina <alex.sherbina.work@gmail.com>
2021-09-14 12:30:55 +0300

    fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -133,56 +133,56 @@ console.log('TEst scriptjs');
 
 
 // ==Chart============================================================//
-// let densityCanvas = document.getElementById("chart-price");
-// Chart.defaults.font.size = 30;
+let densityCanvas = document.getElementById("chart-price");
+Chart.defaults.font.size = 30;
 
 
 
-// let case1 = {
-//   label: 'Изменение цены за месяц',
-//   data: [ 0, 33.5, 80 ],
-//   backgroundColor: 'rgba(48, 207, 208, 0.6)',
-//   borderWidth: 1,
+let case1 = {
+  label: '',
+  data: [ 0, 33.5, 80 ],
+  backgroundColor: 'rgba(48, 207, 208, 0.6)',
+  borderWidth: 1,
 
-// };
+};
 
 
-// let casetData = {
-//   labels: ["Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", "Январь", "Февраль", "Март" , "Апрель", "Май", "Июнь"],
-//   datasets: [case1]
-// };
+let casetData = {
+  labels: ["Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", "Январь", "Февраль", "Март" , "Апрель", "Май", "Июнь"],
+  datasets: [case1]
+};
 
-// let chartOptions = {
-//   scales: {
-//     y: {
-//         ticks: {
+let chartOptions = {
+  scales: {
+    y: {
+        ticks: {
         
-//             callback: function(value, index, values) {
-//                 return  value + '%';
-//             }
-//         }},
-//     xAxes: [{
-//       barPercentage: 1,
-//       categoryPercentage: 0.6
-//     }],   
+            callback: function(value, index, values) {
+                return  value + '%';
+            }
+        }},
+    xAxes: [{
+      barPercentage: 1,
+      categoryPercentage: 0.6
+    }],   
 
-//   },
-//   plugins: {
-//     legend: {
-//         labels: {
-//             font: {
-//                 size: 30
-//             }
-//         }
-//     }
-//     }
-// };
+  },
+  plugins: {
+    legend: {
+        labels: {
+            font: {
+                size: 30
+            }
+        }
+    }
+    }
+};
 
-// let barChart = new Chart(densityCanvas, {
-//   type: 'bar',
-//   data: casetData,
-//   options: chartOptions
-// });
+let barChart = new Chart(densityCanvas, {
+  type: 'bar',
+  data: casetData,
+  options: chartOptions
+});
 
 
 //=====feed-form=====================//
